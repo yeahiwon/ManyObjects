d4e457123e61328e6c2990b28c3d6ecabdb67769
yeahiwon <89372405+yeahiwon@users.noreply.github.com>
2021-11-02 15:43:42 +0900

    array particles

--- Mover.js
+++ Mover.js
@@ -4,22 +4,34 @@ class Mover {
     this.vel = createVector();
     this.acc = createVector();
     this.m = m;
-    
+
   }
-  
+
   applyForce(aForce) {
     let f = p5.Vector.div(aForce, this.m);
     this.acc.add(f);
   }
-  
+
+  attractTo(aPos) {
+    let f = p5.Vector.sub(aPos,this.pos);
+    f.limit(2);
+    this.applyForce(f);
+  }
+
   update() {
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.set(0, 0);
   }
-  
+
   show() {
     fill(0);
     circle(this.pos.x, this.pos.y, this.m);
+  }
+
+  drawOn(c) {
+    c.fill(250, 250, 0, 20);
+    c.noStroke();
+    c.circle(this.pos.x, this.pos.y, this.m);
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,16 +1,40 @@
-let m;
+let m, m2;
+let movers = [];
+let canvas;
 
 function setup() {
   createCanvas(400, 400);
+  canvas = createGraphics(400, 400);
   m = new Mover(50, 50, 10);
+  m2 = new Mover(300, 300, 50);
+
+  for (let i=0; i<10; i++) {
+    let mover = new Mover(random(400),random(400),random(5,50))
+    movers.push(mover);
+  }
 }
 
 function draw() {
   background(220);
-  m.update();
-  m.show();
-  
+  image(canvas, 0, 0);
+  // for (let i=0; (i < movers.length; i++) {
+  //   let mover = movers[i];
+  //   mover.update();
+  //   mover.show();
+  // }
+
+  for (let mover of movers) {
+    mover.update();
+    mover.show();
+    mover.drawOn(canvas);
+  }
+
   if (mouseIsPressed) {
     //make attract to mouse position
+   let mousePos = createVector(mouseX,mouseY);
+
+   for (let mover of movers) {
+     mover.attractTo(mousePos);
+   }
   }
 }
